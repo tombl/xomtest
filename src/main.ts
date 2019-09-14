690f3cf6e90403363d02d314f3fcb8ad87216ad2
Tom S <tombliboo40@gmail.com>
2019-09-14 16:35:03 +0800

    enforce only .ts files

--- src/main.ts
+++ src/main.ts
@@ -82,7 +82,7 @@ pptr.launch({ headless: false }).then(async browser => {
   const logger = new Logger();
   await Promise.all(
     (await fs.readdir(testDir))
-      .filter(file => file[0] !== "_")
+      .filter(file => file[0] !== "_" && file.slice(-3) === "ts")
       .map(file =>
         queue.add(async () => {
           const page = await browser.newPage();
