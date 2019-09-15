479897003f7d33aa2c9553ab264b0bee75da6793
Tom S <tombliboo40@gmail.com>
2019-09-15 14:36:38 +0800

    memoize bundled code

--- src/bundle-lib.ts
+++ src/bundle-lib.ts
@@ -2,22 +2,27 @@ import { rollup } from "rollup";
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 
+let code: string;
+
 export default async function({
   packageJsonPath
 }: {
   packageJsonPath: string;
 }) {
-  const packageJson = require(packageJsonPath);
-  if (packageJson.module === undefined && packageJson.main === undefined) {
-    return "";
+  if (code === undefined) {
+    const packageJson = require(packageJsonPath);
+    if (packageJson.module === undefined && packageJson.main === undefined) {
+      return "";
+    }
+    const bundle = await rollup({
+      external: ["xomtest/runtime"],
+      input: packageJson.module || packageJson.main,
+      plugins: [commonjs(), nodeResolve()]
+    });
+    const { output } = await bundle.generate({
+      format: "esm"
+    });
+    code = output[0].code;
   }
-  const bundle = await rollup({
-    external: ["xomtest/runtime"],
-    input: packageJson.module || packageJson.main,
-    plugins: [commonjs(), nodeResolve()]
-  });
-  const { output } = await bundle.generate({
-    format: "esm"
-  });
-  return output[0].code;
+  return code;
 }
